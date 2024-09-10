e31e4ffd9023065e25297d499e4b30c5ea46dc68
Cooper <simpson.r.cooper@gmail.com>
2024-09-10 09:55:26 -0700

    Changing neuron transport jobs.

---
+++ convert.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pathlib
+from natsort import natsorted
+
+
+path = pathlib.Path("data/ionization/")
+
+for i, file in enumerate(natsorted(path.glob("*.txt"))):
+    #convert to numpy
+    arr = np.loadtxt(file)
+
+    #extract channels
+    arr = arr[:,[0,1,8]]
+
+    #save
+    np.save(path/f"features_{i}.npy", arr)
