6b9032716e143d79e44a04a25d599e6ca4c09c5f
Cooper <simpson.r.cooper@gmail.com>
2024-12-08 21:13:18 -0800

    New subsample NT jobs.

--- transfer.sh
+++ transfer.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [$1=="blanca"]
+if [ $1 == "blanca" ]
 then
     sshPATH=cosi1728@login.rc.colorado.edu:/projects/cosi1728/Implicit-Neural-Compression
-elif [$1=="hyak"]
+elif [ $1 == "hyak" ]
 then
     sshPATH=rscooper@klone.hyak.uw.edu:projects/Implicit-Neural-Compression
 fi
 
-if [$2=="scp"]
+if [ $2 == "scp" ]
 then
-    scp -r $sshPATH/lightning_logs/$1 lightning_logs/$2
-elif [$2=="rsync"]
+    scp -r $sshPATH/lightning_logs/$3 lightning_logs/$4
+elif [ $2 = "rsync" ]
 then
-    rsync -rP $sshPATH/lightning_logs/$1 lightning_logs/$2
+    rsync -rP $sshPATH/lightning_logs/$3 lightning_logs/$4
 fi
